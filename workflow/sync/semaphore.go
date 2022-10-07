4c2f7ad484058299408ca7a2dec168f341d77213
Robert Kotcher <rkotcher+1@gmail.com>
2022-10-07 14:07:39 +0000

    feat: Implement RebalanceQueue
    Signed-off-by: Robert Kotcher <rkotcher+1@gmail.com>

--- workflow/sync/semaphore.go
+++ workflow/sync/semaphore.go
@@ -6,36 +6,43 @@ import (
 	"sync"
 	"time"
 
-	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
-	"github.com/aws/smithy-go/ptr"
 	log "github.com/sirupsen/logrus"
 	sema "golang.org/x/sync/semaphore"
 )
 
+// OrderedItems is kindof like a queue, but removes the stricter set of requirements
+// than the word queue would implies
+type OrderedItems interface {
+	pop() *item
+	add(key Key, priority int32, creationTime time.Time)
+	remove(key Key)
+	items() []*item
+}
+
 type PrioritySemaphore struct {
 	name         string
 	limit        int
-	pending      *priorityQueue
+	pending      OrderedItems
 	semaphore    *sema.Weighted
 	lockHolder   map[string]bool
 	lock         *sync.Mutex
 	nextWorkflow NextWorkflow
 	log          *log.Entry
+	wrappedPeek  func(s *PrioritySemaphore, n int) []*item
 	getWorkflow  GetWorkflow
 }
 
 var _ Semaphore = &PrioritySemaphore{}
 
-func NewSemaphore(name string, limit int, nextWorkflow NextWorkflow, lockType string, getWorkflow GetWorkflow) *PrioritySemaphore {
+func NewSemaphore(name string, limit int, nextWorkflow NextWorkflow, lockType string, orderedItems OrderedItems) *PrioritySemaphore {
 	return &PrioritySemaphore{
 		name:         name,
 		limit:        limit,
-		pending:      &priorityQueue{itemByKey: make(map[string]*item)},
+		pending:      orderedItems,
 		semaphore:    sema.NewWeighted(int64(limit)),
 		lockHolder:   make(map[string]bool),
 		lock:         &sync.Mutex{},
 		nextWorkflow: nextWorkflow,
-		getWorkflow:  getWorkflow,
 		log: log.WithFields(log.Fields{
 			lockType: name,
 		}),
@@ -52,7 +59,7 @@ func (s *PrioritySemaphore) getLimit() int {
 
 func (s *PrioritySemaphore) getCurrentPending() []string {
 	var keys []string
-	for _, item := range s.pending.items {
+	for _, item := range s.pending.items() {
 		keys = append(keys, item.key)
 	}
 	return keys
@@ -100,7 +107,7 @@ func (s *PrioritySemaphore) release(key string) bool {
 		s.semaphore.Release(1)
 		availableLocks := s.limit - len(s.lockHolder)
 		s.log.Infof("Lock has been released by %s. Available locks: %d", key, availableLocks)
-		if s.pending.Len() > 0 {
+		if len(s.pending.items()) > 0 {
 			s.notifyWaiters()
 		}
 	}
@@ -110,35 +117,15 @@ func (s *PrioritySemaphore) release(key string) bool {
 // notifyWaiters enqueues the next N workflows who are waiting for the semaphore to the workqueue,
 // where N is the availability of the semaphore. If semaphore is out of capacity, this does nothing.
 func (s *PrioritySemaphore) notifyWaiters() {
-	totalTriggers := s.limit - len(s.lockHolder)
-	countTriggers := 0
-
-	holding := s.getCurrentHolders()
-	pending := s.getCurrentPending()
-
-	for idx := 0; idx < s.pending.Len(); idx++ {
-		item := s.pending.items[idx]
-		shouldTrigger, err := s.checkStrategy(holding, pending, item.key)
-		if err != nil {
-			log.Errorf("got an error while checking strategy: %s", err)
-		}
-		if shouldTrigger {
-			wfKey := workflowKey(item)
-			s.log.Debugf("enqueue the workflow %s", wfKey)
-			s.nextWorkflow(wfKey)
-			countTriggers += 1
-			if countTriggers == totalTriggers {
-				break
-			}
-
-			// adjust pending and holding
-			for i, v := range pending {
-				if v == item.key {
-					pending = append(pending[:i], pending[i+1:]...)
-				}
-			}
-			holding = append(holding, item.key)
-		}
+	triggerCount := s.limit - len(s.lockHolder)
+	if len(s.pending.items()) < triggerCount {
+		triggerCount = len(s.pending.items())
+	}
+	items := s.wrappedPeek(s, triggerCount)
+	for _, item := range items {
+		wfKey := workflowKey(item)
+		s.log.Debugf("Enqueue the workflow %s", wfKey)
+		s.nextWorkflow(wfKey)
 	}
 }
 
@@ -194,91 +181,6 @@ func isSameWorkflowNodeKeys(firstKey, secondKey string) bool {
 	return firstItems[1] == secondItems[1]
 }
 
-func getRebalanceKey(wf *wfv1.Workflow, lockRefKey string) *string {
-	for _, t := range wf.Spec.Templates {
-		if t.Synchronization != nil && t.Synchronization.Semaphore != nil && t.Synchronization.Semaphore.ConfigMapKeyRef.Key == lockRefKey {
-			return t.Synchronization.Semaphore.RebalanceKey
-		}
-	}
-	return nil
-}
-
-// checkStrategy tells us if we can proceed to try and acquire using primitive semaphore
-func (s *PrioritySemaphore) checkStrategy(holding, pending []string, requesterKey string) (bool, error) {
-	items := strings.Split(requesterKey, "/")
-	wfKey := items[0] + "/" + items[1]
-	lockNameItems := strings.Split(s.name, "/")
-	lockRefKey := lockNameItems[len(lockNameItems)-1]
-
-	wf, err := s.getWorkflow(wfKey)
-	if err != nil {
-		return false, fmt.Errorf("could not find workflow from key %s", wfKey)
-	}
-
-	// if rebalance key does not exist, always proceed (try acquire next)
-	thisRebalanceKey := getRebalanceKey(wf, lockRefKey)
-	if thisRebalanceKey == nil {
-		return true, nil
-	}
-
-	// count the number of active rebalance keys equaling "thisRebalanceKey"
-	thisRebalanceKeyCount := 0
-
-	// nUsers : determine all rebalance keys (pending + holding + this wf)
-	allRebalanceKeys := []string{*thisRebalanceKey}
-	for _, resourceKey := range pending {
-		items := strings.Split(resourceKey, "/")
-		wfKey := items[0] + "/" + items[1]
-		wf, err := s.getWorkflow(wfKey)
-		if err != nil {
-			return false, fmt.Errorf("could not find workflow %s but it should exist", wfKey)
-		}
-
-		rk := getRebalanceKey(wf, lockRefKey)
-		if rk == nil {
-			rk = ptr.String("nil")
-		}
-		foundKey := false
-		for _, k := range allRebalanceKeys {
-			if k == *rk {
-				foundKey = true
-			}
-		}
-		if !foundKey {
-			allRebalanceKeys = append(allRebalanceKeys, *rk)
-		}
-	}
-	for _, resourceKey := range holding {
-		items := strings.Split(resourceKey, "/")
-		wfKey := items[0] + "/" + items[1]
-		wf, err := s.getWorkflow(wfKey)
-		if err != nil {
-			return false, fmt.Errorf("could not find workflow %s but it should exist", wfKey)
-		}
-
-		rk := getRebalanceKey(wf, lockRefKey)
-		if rk == nil {
-			rk = ptr.String("nil")
-		}
-		if *rk == *thisRebalanceKey {
-			thisRebalanceKeyCount += 1
-		}
-		foundKey := false
-		for _, k := range allRebalanceKeys {
-			if k == *rk {
-				foundKey = true
-			}
-		}
-		if !foundKey {
-			allRebalanceKeys = append(allRebalanceKeys, *rk)
-		}
-	}
-
-	nLocksPerUser := float64(s.limit) / float64(len(allRebalanceKeys))
-
-	return float64(thisRebalanceKeyCount) < nLocksPerUser, nil
-}
-
 func (s *PrioritySemaphore) tryAcquire(holderKey string) (bool, string) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
@@ -290,37 +192,18 @@ func (s *PrioritySemaphore) tryAcquire(holderKey string) (bool, string) {
 
 	waitingMsg := fmt.Sprintf("Waiting for %s lock. Lock status: %d/%d", s.name, s.limit-len(s.lockHolder), s.limit)
 
-	wfKey := workflowKey(&item{key: holderKey})
-	wf, err := s.getWorkflow(wfKey)
-	if err != nil {
-		return false, fmt.Sprintf("could not find requesting workflow: %s", err.Error())
-	}
-	lockNameItems := strings.Split(s.name, "/")
-	lockRefKey := lockNameItems[len(lockNameItems)-1]
-
-	proceed := true
-	if getRebalanceKey(wf, lockRefKey) == nil {
-		// Check whether requested holdkey is in front of priority queue.
-		// If it is in front position, it will allow to acquire lock.
-		// If it is not a front key, it needs to wait for its turn.
-		if s.pending.Len() > 0 {
-			item := s.pending.peek()
-			if holderKey != "" && !isSameWorkflowNodeKeys(holderKey, item.key) {
-				// Enqueue the front workflow if lock is available
-				if len(s.lockHolder) < s.limit {
-					s.nextWorkflow(workflowKey(item))
-				}
-				return false, waitingMsg
+	if len(s.pending.items()) > 0 {
+		item := s.wrappedPeek(s, 1)[0]
+		if holderKey != "" && !isSameWorkflowNodeKeys(holderKey, item.key) {
+			// Enqueue the front workflow if lock is available
+			if len(s.lockHolder) < s.limit {
+				s.nextWorkflow(workflowKey(item))
 			}
-		}
-	} else {
-		proceed, err = s.checkStrategy(s.getCurrentHolders(), s.getCurrentPending(), holderKey)
-		if err != nil {
-			return false, fmt.Sprintf("could not check strategy: %s", err.Error())
+			return false, waitingMsg
 		}
 	}
 
-	if proceed && s.acquire(holderKey) {
+	if s.acquire(holderKey) {
 		s.pending.pop()
 		s.log.Infof("%s acquired by %s. Lock availability: %d/%d", s.name, holderKey, s.limit-len(s.lockHolder), s.limit)
 		s.notifyWaiters()
